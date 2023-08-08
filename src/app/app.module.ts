f9fc8ad8fffe009b4b87692986bbf697a76f71e9
Carlton Li <gregor.goff@gmail.com>
2023-08-08 17:26:14 +0800

    added docker file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdyenComponentComponent } from './adyen-component/adyen-component.component';
+import { PaymentWidgetComponent } from './payment-widget/payment-widget.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdyenComponentComponent
+    AdyenComponentComponent,
+    PaymentWidgetComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/payment-widget/payment-widget.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-widget',
+  templateUrl: './payment-widget.component.html',
+  styleUrls: ['./payment-widget.component.css']
+})
+export class PaymentWidgetComponent {
+
+}
